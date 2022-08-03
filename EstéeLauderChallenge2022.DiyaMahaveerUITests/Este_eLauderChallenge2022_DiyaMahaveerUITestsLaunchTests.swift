0b77328e2b1d7e26de4c80206a62966a5c343ba5
Diya Mahaveer <diyamahaveer@gmail.com>
2022-08-03 13:16:51 -0400

    Initial Commit

---
+++ EstéeLauderChallenge2022.DiyaMahaveerUITests/Este_eLauderChallenge2022_DiyaMahaveerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Este_eLauderChallenge2022_DiyaMahaveerUITestsLaunchTests.swift
+//  EstéeLauderChallenge2022.DiyaMahaveerUITests
+//
+//  Created by Diya Mahaveer on 8/3/22.
+//
+
+import XCTest
+
+class Este_eLauderChallenge2022_DiyaMahaveerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
